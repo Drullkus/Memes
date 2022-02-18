305703ccb0f8ef6fc82891a31995c7b713445496
Drullkus <failbookftw@gmail.com>
2022-02-17 16:35:14 -0800

    Init Git Repo

---
+++ src/test/java/us/drullk/memes/MemesApplicationTests.java
@@ -0,0 +1,13 @@
+package us.drullk.memes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
